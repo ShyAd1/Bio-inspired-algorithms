45b44daedb80af9de426608fc655a20c54f714d6
Adair Hernandez <46034218+ShyAd1@users.noreply.github.com>
2026-03-07 23:29:54 -0600

    Practica1_knapsack

--- Practica1_knapsack/main.py
+++ Practica1_knapsack/main.py
@@ -17,17 +17,20 @@ MAX_PARA_CADA_OBJETO = 10
 # Objeto 7: Nosebleed Nougat (1, 2)
 OBJETOS = [(4, 10), (2, 8), (5, 12), (5, 6), (2, 3), (1.5, 2), (1, 2)]
 UMBRAL_CRUZA = 0.5
-PROBABILIDADES = [0] * TAM_POBALACION
-PADRES_SELECCIONADOS = (
-    []
-)  # Tupla (padre1, padre2) para cada par de padres seleccionados
+PROBABILIDADES = [
+    0
+] * TAM_POBALACION  # Lista de probabilidades acumuladas para la selección por ruleta
 VARIABLES_ALEATORIAS = [0] * len(
     OBJETOS
 )  # Lista de variables aleatorias generadas para la selección de padres
 
+# Seed para reproducibilidad
+# random.seed(1143417438)
+
 # Restricciones
 # 1.- Al menos debe de haber 3 Love Potions y 2 Skiving Snackbox en la mochila
-# Por lo que se generaran valores aleatorios entre 3 y 10 para el objeto 2 y entre 2 y 10 para el objeto 4 para los demas sera entre 0 y 10
+# por lo que se generaran valores aleatorios entre 3 y 10 para el objeto 2 y entre 2 y 10
+# para el objeto 4 para los demas sera entre 0 y 10
 # 2.- Seleccion de padres por metodo de ruleta
 # 3.- Cruza por metodo de cruza uniforme
 # 4.- Mutacion por metodo de mutacion uniforme
@@ -35,8 +38,8 @@ VARIABLES_ALEATORIAS = [0] * len(
 
 
 def generar_individuo():
-    # Generar un individuo aleatorio que cumpla las restricciones de al menos 3 Love Potions y
-    # 2 Skiving Snackbox y que no exceda la capacidad de la mochila
+    # Generar un individuo aleatorio que cumpla las restricciones de al menos
+    # 3 Love Potions y 2 Skiving Snackbox y que no exceda la capacidad de la mochila
     while True:
         individuo = [
             random.randint(0, MAX_PARA_CADA_OBJETO),  # Decoy Detonators
@@ -56,25 +59,23 @@ def calcular_fitness(individuo):
     peso_total = sum(individuo[i] * OBJETOS[i][0] for i in range(len(OBJETOS)))
     valor_total = sum(individuo[i] * OBJETOS[i][1] for i in range(len(OBJETOS)))
     if peso_total > CAP_MOCHILA:
-        return 0  # Penalización por exceder la capacidad
-    return valor_total
+        return 0, peso_total  # Penalización por exceder la capacidad
+    return valor_total, peso_total
 
 
 def tabla_fitness(poblacion):
-    print("Individuo\t\t\tPeso Total\tValor Total\tFitness")
+    print("Individuo\t\t\tPeso Total\tFitness")
     for individuo in poblacion:
-        peso_total = sum(individuo[i] * OBJETOS[i][0] for i in range(len(OBJETOS)))
-        valor_total = sum(individuo[i] * OBJETOS[i][1] for i in range(len(OBJETOS)))
-        fitness = calcular_fitness(individuo)
-        print(f"{individuo}\t\t{peso_total}\t\t{valor_total}\t\t{fitness}")
+        fitness, peso_total = calcular_fitness(individuo)
+        print(f"{individuo}\t\t{peso_total}\t\t{fitness}")
 
 
 def tabla_ruleta(poblacion):
-    fitness_total = sum(calcular_fitness(individuo) for individuo in poblacion)
+    fitness_total = sum(calcular_fitness(individuo)[0] for individuo in poblacion)
     print("Individuo\t\t\tFitness\t\tProbabilidad\tProbabilidad Acumulada")
     prob_acumulada = 0
     for individuo in poblacion:
-        fitness = calcular_fitness(individuo)
+        fitness, _ = calcular_fitness(individuo)
         probabilidad = fitness / fitness_total if fitness_total > 0 else 0
         prob_acumulada += probabilidad
         PROBABILIDADES[poblacion.index(individuo)] = prob_acumulada
@@ -86,45 +87,72 @@ def variable_aleatoria():
 
 
 def cruza_uniforme(padre1, padre2):
-    hijo1 = padre1.copy()
-    hijo2 = padre2.copy()
+    # Realizar cruza uniforme entre dos padres para generar dos hijos dentro de
+    # la maxima capacidad de la mochila y cumpliendo las restricciones de al
+    # menos 3 Love Potions y 2 Skiving Snackbox
+    while True:
+        hijo1 = padre1.copy()
+        hijo2 = padre2.copy()
 
-    if variable_aleatoria() > PROB_CRUZA:
-        # No se realiza cruza, los hijos son iguales a los padres,
-        # basicamente pasan los padres a la siguiente generacion sin cambios
-        return hijo1, hijo2
+        if variable_aleatoria() > PROB_CRUZA:
+            # No se realiza cruza, los hijos son iguales a los padres,
+            # basicamente pasan los padres a la siguiente generacion sin cambios
+            return hijo1, hijo2
 
-    for i in range(len(padre1)):
-        if VARIABLES_ALEATORIAS[i] <= UMBRAL_CRUZA:
-            hijo1[i] = padre1[i]
-        else:
-            hijo1[i] = padre2[i]
+        VARIABLES_ALEATORIAS = [0] * len(
+            OBJETOS
+        )  # Reiniciar variables aleatorias para cruza
+        for i in range(0, len(OBJETOS)):
+            VARIABLES_ALEATORIAS[i] = variable_aleatoria()
 
-    for i in range(len(padre2)):
-        if VARIABLES_ALEATORIAS[i] <= UMBRAL_CRUZA:
-            hijo2[i] = padre2[i]
-        else:
-            hijo2[i] = padre1[i]
+        # Cruza uniforme
+        for i in range(len(padre1)):
+            if VARIABLES_ALEATORIAS[i] <= UMBRAL_CRUZA:
+                hijo1[i] = padre1[i]
+                hijo2[i] = padre2[i]
+            else:
+                hijo1[i] = padre2[i]
+                hijo2[i] = padre1[i]
 
-    for i in range(0, len(OBJETOS)):
-        VARIABLES_ALEATORIAS[i] = variable_aleatoria()
-    print("\nVariables aleatorias para mutacion del hijo 1:")
-    print(VARIABLES_ALEATORIAS)
+        VARIABLES_ALEATORIAS = [0] * len(
+            OBJETOS
+        )  # Reiniciar variables aleatorias para mutacion hijo 1
 
-    print(f"Hijo 1 antes de mutacion: {hijo1}")
+        # Mutacion uniforme para el hijo 1
+        for i in range(0, len(OBJETOS)):
+            VARIABLES_ALEATORIAS[i] = variable_aleatoria()
+        # print("\nVariables aleatorias para mutacion del hijo 1:")
+        # print(VARIABLES_ALEATORIAS)
 
-    hijo1 = mutacion_uniforme(hijo1)
+        # print(f"Hijo 1 antes de mutacion: {hijo1}")
 
-    for i in range(0, len(OBJETOS)):
-        VARIABLES_ALEATORIAS[i] = variable_aleatoria()
-    print("\nVariables aleatorias para mutacion del hijo 2:")
-    print(VARIABLES_ALEATORIAS)
+        hijo1 = mutacion_uniforme(hijo1)
 
-    print(f"Hijo 2 antes de mutacion: {hijo2}")
+        # print(f"Hijo 1 despues de mutacion: {hijo1}")
 
-    hijo2 = mutacion_uniforme(hijo2)
+        VARIABLES_ALEATORIAS = [0] * len(
+            OBJETOS
+        )  # Reiniciar variables aleatorias para mutacion del hijo 2
 
-    return hijo1, hijo2
+        # Mutacion uniforme para el hijo 2
+        for i in range(0, len(OBJETOS)):
+            VARIABLES_ALEATORIAS[i] = variable_aleatoria()
+        # print("\nVariables aleatorias para mutacion del hijo 2:")
+        # print(VARIABLES_ALEATORIAS)
+
+        # print(f"Hijo 2 antes de mutacion: {hijo2}")
+
+        hijo2 = mutacion_uniforme(hijo2)
+
+        # print(f"Hijo 2 despues de mutacion: {hijo2}")
+
+        # Verificar que los hijos cumplan con la restricción de capacidad
+        peso_hijo1 = sum(hijo1[i] * OBJETOS[i][0] for i in range(len(OBJETOS)))
+        peso_hijo2 = sum(hijo2[i] * OBJETOS[i][0] for i in range(len(OBJETOS)))
+
+        if peso_hijo1 <= CAP_MOCHILA and peso_hijo2 <= CAP_MOCHILA:
+            return hijo1, hijo2
+        # Si no cumplen, el while True vuelve a intentar con nueva cruza y mutación
 
 
 def mutacion_uniforme(individuo):
@@ -137,6 +165,31 @@ def mutacion_uniforme(individuo):
             else:
                 individuo[i] = random.randint(0, MAX_PARA_CADA_OBJETO)
     return individuo
+
+
+def seleccionar_sobreviviente(padre1, padre2, hijo1, hijo2):
+    fitness_padre1, _ = calcular_fitness(padre1)
+    fitness_padre2, _ = calcular_fitness(padre2)
+    fitness_hijo1, _ = calcular_fitness(hijo1)
+    fitness_hijo2, _ = calcular_fitness(hijo2)
+
+    # Ordenar de mayor a menor y seleccionar a los dos mejores individuos para
+    # la siguiente generación
+    individuos = [padre1, padre2, hijo1, hijo2]
+    fitnesses = [fitness_padre1, fitness_padre2, fitness_hijo1, fitness_hijo2]
+
+    # Ordenar los individuos por fitness de mayor a menor
+    individuos_ordenados = [
+        x
+        for _, x in sorted(
+            zip(fitnesses, individuos), key=lambda pair: pair[0], reverse=True
+        )
+    ]
+
+    return (
+        individuos_ordenados[0],
+        individuos_ordenados[1],
+    )  # Retornar los dos mejores individuos
 
 
 if __name__ == "__main__":
@@ -158,67 +211,58 @@ if __name__ == "__main__":
         print("\nTabla de selección por ruleta:")
         tabla_ruleta(poblacion)
 
-        # Selección de padres por random.uniform entre 0 y 1 y comparando con las probabilidades acumuladas
-        # en forma tupla (padre1, padre2) para cada par de padres seleccionados
-        for i in range(TAM_POBALACION):
-            r = random.uniform(0, 1)
-            print(f"Random: {r:.4f}")
-            for j in range(TAM_POBALACION):
-                if r <= PROBABILIDADES[j]:
-                    PADRES_SELECCIONADOS.append(poblacion[j])
+        nueva_poblacion = []
+
+        while (
+            len(nueva_poblacion) < TAM_POBALACION
+        ):  # Generar hijos hasta tener el doble de la población actual
+            r1 = variable_aleatoria()
+            r2 = variable_aleatoria()
+
+            padre1 = None
+            padre2 = None
+
+            # Seleccionar 2 padres utilizando la tabla de ruleta sin que se
+            # seleccione el mismo padre dos veces
+            for i in range(TAM_POBALACION):
+                if r1 <= PROBABILIDADES[i] and padre1 is None:
+                    padre1 = poblacion[i]
+                if r2 <= PROBABILIDADES[i] and padre2 is None:
+                    padre2 = poblacion[i]
+                if padre1 is not None and padre2 is not None:
                     break
 
-        print("\nPadres seleccionados:")
-        for i in range(0, len(PADRES_SELECCIONADOS), 2):
-            print(
-                f"Padre 1: {PADRES_SELECCIONADOS[i]}, Padre 2: {PADRES_SELECCIONADOS[i+1]}"
-            )
-
-        for i in range(0, len(OBJETOS)):
-            VARIABLES_ALEATORIAS[i] = variable_aleatoria()
-        print("\nVariables aleatorias para cruza:")
-        print(VARIABLES_ALEATORIAS)
-
-        # Cruza por método de cruza uniforme y mutacion uniforme
-        poblacion = []
-        for i in range(0, len(PADRES_SELECCIONADOS), 2):
-            padre1 = PADRES_SELECCIONADOS[i]
-            padre2 = PADRES_SELECCIONADOS[i + 1]
+            # Cruza uniforme para generar dos hijos
             hijo1, hijo2 = cruza_uniforme(padre1, padre2)
 
-            poblacion.append(hijo1)
-            poblacion.append(hijo2)
-
-        print("\nPoblación después de cruza uniforme:")
-        for individuo in poblacion:
-            print(individuo)
-
-        # Evaluar fitness de la nueva población
-        print("\nTabla de fitness de la nueva población:")
-        tabla_fitness(poblacion)
-
-        # Selección de sobreviviente por método generacional con remplazo del padre más débil
-
-        # Ordenar los padres por fitness de menor a mayor
-        padres_ordenados = sorted(
-            PADRES_SELECCIONADOS, key=lambda ind: calcular_fitness(ind)
-        )
-
-        # Eliminar los hijos que superan la capacidad de la mochila y remplazar por el padre más débil
-        nueva_poblacion = []
-        for hijo in poblacion:
-            if calcular_fitness(hijo) > 0:  # Solo agregar hijos válidos
-                nueva_poblacion.append(hijo)
+            # Selección de sobrevivientes por método generacional con remplazo del
+            # padre más débil pero si la cruza da que no se realiza, entonces los
+            # hijos son iguales a los padres y causaria algo erroneo en la seleccion
+            # de sobrevivientes, por lo que se verifica que los hijos sean diferentes
+            # a los padres
+            if (
+                hijo1 != padre1
+                and hijo1 != padre2
+                and hijo2 != padre1
+                and hijo2 != padre2
+            ):
+                sobreviviente1, sobreviviente2 = seleccionar_sobreviviente(
+                    padre1, padre2, hijo1, hijo2
+                )
+                nueva_poblacion.append(sobreviviente1)
+                nueva_poblacion.append(sobreviviente2)
             else:
-                nueva_poblacion.append(
-                    padres_ordenados[0]
-                )  # Reemplazar por el padre más débil
+                nueva_poblacion.append(padre1)
+                nueva_poblacion.append(padre2)
 
-        print("\nNueva población después de selección de sobrevivientes:")
-        for individuo in nueva_poblacion:
-            print(individuo)
+        poblacion = nueva_poblacion[
+            :TAM_POBALACION
+        ]  # Mantener solo el tamaño de población definido
 
-        poblacion = nueva_poblacion
+        print(
+            "\nPoblación después de cruza uniforme mutada y seleccion de sobrevivientes:"
+        )
+        tabla_fitness(poblacion)
 
         # Resetear variables para la siguiente generación
         PROBABILIDADES = [0] * TAM_POBALACION
