62ccc6d35a586efa1edebe12102b1bc2c189ab1b
Adair Hernandez <46034218+ShyAd1@users.noreply.github.com>
2026-06-24 19:39:53 -0600

    Proyecto v2.4 final

--- Proyecto/core/aco.py
+++ Proyecto/core/aco.py
@@ -124,34 +124,6 @@ class ACOSolver:
 
         return nodos_posibles[indice_elegido]
 
-    # def _walk(self, start: str) -> Optional[tuple[list[str], float]]:
-    #     """
-    #     One ant walk from `start` until it reaches an E node or gets stuck.
-    #     Returns (path, distance) or None if stuck.
-    #     Max steps = 2 * number of nodes to avoid infinite loops.
-    #     """
-    #     path = [start]
-    #     visited = {start}
-    #     total_d = 0.0
-    #     max_steps = len(self.all_node_ids) * 2
-
-    #     for _ in range(max_steps):
-    #         current = path[-1]
-    #         if current in self.exit_ids:
-    #             return path, total_d
-
-    #         nxt = self._next_node(current, visited)
-    #         if nxt is None:
-    #             return None  # stuck
-
-    #         # get distance for this edge
-    #         d = next((dist for nid, dist in self.graph.get(current, []) if nid == nxt), 1.0)
-    #         path.append(nxt)
-    #         visited.add(nxt)
-    #         total_d += d
-
-    #     return None  # exceeded max steps
-
     def _deposit(self, path: list[str], dist: float):
         if dist == 0:
             return
